97f34d7121401f18ca1dd39c6328102cfbbc1201
akshay <pandeya@vmcmail.com>
2018-03-19 16:03:43 -0500

    Files Added

---
+++ table_view_api/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  table_view_api
+//
+//  Created by Akshay on 3/19/18.
+//  Copyright © 2018 Akshay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+    let flightName = ["Delta","United","American"]
+    let price = [177,190,201]
+
+    @IBOutlet weak var flightTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        flightTableView.delegate = self
+        flightTableView.dataSource = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flightName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = flightTableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = flightName[indexPath.row]
+        cell?.detailTextLabel?.text = "\(price[indexPath.row])"
+        
+        return cell!
+    }
+
+   
+
+
+}
+
