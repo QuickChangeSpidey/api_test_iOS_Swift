74e64e22c3760225300de672642179e2805444c3
akshay <pandeya@vmcmail.com>
2018-03-21 17:47:45 -0500

    UI Changes table cell --- needs work --- next screen feature + UILabel for text pending

--- table_view_api/ViewController.swift
+++ table_view_api/ViewController.swift
@@ -21,17 +21,48 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
     }
     
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell,
+                   forRowAt indexPath: IndexPath)
+    {
+        if (indexPath.row % 2 == 0)
+        {
+            cell.backgroundColor = colorForIndex(index: indexPath.row)
+        } else {
+            cell.backgroundColor = UIColor.white
+        }
+    }
+    
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.flights.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = flightTableView.dequeueReusableCell(withIdentifier: "cell")
-        cell?.textLabel?.text = "test"
-        cell?.detailTextLabel?.text = "$0"
         
-        return cell!
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? FlightTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+        }
+        let flight = flights[indexPath.row]
+        
+        cell.carrierCellText.text = flight.Carrier
+        cell.destinationCellText.text = flight.Dest
+        cell.departureCellText.text = flight.SchedDepTime
+        cell.flightIdCellText.text = flight.FltId
+        cell.sourceCellText.text = flight.Orig
+        cell.statusCellText.text = flight.Status
+        
+        return cell
     }
+    
+    func colorForIndex(index: Int) -> UIColor
+    {
+        let itemCount = self.flights.count - 1
+        return UIColor(red: 0.9137, green: 0.9294, blue: 0, alpha: 1)
+    }
+    
+    
+    
 }
 
 
