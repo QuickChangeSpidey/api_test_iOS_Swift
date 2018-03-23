c02e85876e44f0f12e98334e25301f9943a46aa2
QuickChangeSpidey <n.akshay.pandey@gmail.com>
2018-03-22 23:24:19 -0500

    SPINNER ADDED

--- table_view_api/SearchViewController.swift
+++ table_view_api/SearchViewController.swift
@@ -10,10 +10,6 @@ import Foundation
 import UIKit
 
 class SearchViewController : UIViewController{
-    
-   
-    
-    
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var minutesBehind: UITextField!
     @IBOutlet weak var minutesAhead: UITextField!
@@ -27,6 +23,7 @@ class SearchViewController : UIViewController{
     
     @IBAction func searchAction(_ sender: Any) {
         
+        let sv = UIViewController.displaySpinner(onView: self.view)
         let airportName: String = airportCode.text!
         let minutesBefore: String = minutesBehind.text!
         let minutesAfter: String = minutesAhead.text!
@@ -34,6 +31,7 @@ class SearchViewController : UIViewController{
         FlightWebService().getFlightData(for: airportName, minutesBehind: minutesBefore, minutesAhead: minutesAfter, completion: { data in
             self.flights = data
             self.performSegue(withIdentifier: "goToSearch", sender: self)
+            UIViewController.removeSpinner(spinner: sv)
 
         })
     }
@@ -42,5 +40,28 @@ class SearchViewController : UIViewController{
         // Create a new variable to store the instance of PlayerTableViewController
         let destinationVC = segue.destination as! ViewController
         destinationVC.flights = self.flights
+    }
+}
+
+extension UIViewController {
+    class func displaySpinner(onView : UIView) -> UIView {
+        let spinnerView = UIView.init(frame: onView.bounds)
+        spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        let ai = UIActivityIndicatorView.init(activityIndicatorStyle: .whiteLarge)
+        ai.startAnimating()
+        ai.center = spinnerView.center
+        
+        DispatchQueue.main.async {
+            spinnerView.addSubview(ai)
+            onView.addSubview(spinnerView)
+        }
+        
+        return spinnerView
+    }
+    
+    class func removeSpinner(spinner :UIView) {
+        DispatchQueue.main.async {
+            spinner.removeFromSuperview()
+        }
     }
 }
